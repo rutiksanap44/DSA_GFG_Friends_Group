85be64972a2b918882e03fbae541ab767c5b1d86
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-25 20:31:19 +0530

    Leader Problem Solved

--- leader.js
+++ leader.js
@@ -1,1 +1,18 @@
-let arr = [3,4,6,7,9,10,2,4,1];
+function leader(arr) {
+    let newArr = [];
+    let tempMax = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (i == arr.length - 1) {
+            newArr.push(arr[i]);
+            tempMax = arr[i];
+            continue;
+        }
+        if(arr[i]>=tempMax){
+            tempMax = arr[i];
+            newArr.unshift(tempMax);
+        }
+    }
+    return newArr;
+}
+let arr = [1,2,3,4,0];
+console.log(leader(arr));
